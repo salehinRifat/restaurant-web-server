12f02c3f737e9c4c3a016a745629b1ef0f055df8
Salehin <salehinrifat2@gmail.com>
2024-01-21 16:24:52 +0600

    Added with mongodb server

--- index.js
+++ index.js
@@ -12,6 +12,40 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@salehinrifat1.7tmx0zj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const menu = client.db("bistroDB").collection("menu");
+        app.get('/menu', async (req, res) => {
+            const cursor = menu.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
